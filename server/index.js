c0eb6c7e7288a8f7f01efed53b0f5a37f9dee592
Thomas Preusse <tpreusse@users.noreply.github.com>
2017-01-09 00:17:29 +0100

    remove fallback, build already includes public folder

--- server/index.js
+++ server/index.js
@@ -1,6 +1,6 @@
+const path = require('path')
 const express = require('express')
 const bodyParser = require('body-parser')
-const fallback = require('express-history-api-fallback')
 require('dotenv').config()
 
 // connect to mongo and load models
@@ -9,16 +9,11 @@ const mongoose = require('./models').connect(process.env.MONGO_URL)
 const PORT = 4000
 const app = express()
 
-var clientBuild = __dirname+'/../build/'
-app.use(express.static(clientBuild))
-app.use(express.static(__dirname+'/../public/'))
+app.use(express.static(path.join(__dirname, '..', 'build')))
 app.use(require('body-parser').json())
 
 // api routes
 app.use('/api', require('./routes/api'))
-
-//use fallback to make client side routes work
-app.use(fallback('index.html', { root: clientBuild }))
 
 app.listen(PORT, () => {
   console.log(`Construction listening on port ${PORT}!`)
