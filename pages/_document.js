3add1a67b8747ec251e1cc68b67f1fd5cb772d2d
Thomas Preusse <tpreusse@users.noreply.github.com>
2017-01-29 21:07:40 +0100

    add piwik

--- pages/_document.js
+++ pages/_document.js
@@ -25,6 +25,20 @@ export default class MyDocument extends Document {
         <body>
           <Main />
           <NextScript />
+          <script dangerouslySetInnerHTML={{__html: `var _paq = _paq || [];
+            // tracker methods like "setCustomDimension" should be called before "trackPageView"
+            _paq.push(['trackPageView']);
+            _paq.push(['enableLinkTracking']);
+            (function() {
+              var u="//piwik.project-r.construction/";
+              _paq.push(['setTrackerUrl', u+'piwik.php']);
+              _paq.push(['setSiteId', '1']);
+              var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
+              g.type='text/javascript'; g.async=true; g.defer=true; g.src=u+'piwik.js'; s.parentNode.insertBefore(g,s);
+            })();`}} />
+          <noscript>
+            <img src='https://piwik.project-r.construction/piwik.php?idsite=1&rec=1' style={{border:0}} alt='' />
+          </noscript>
         </body>
       </html>
     )
