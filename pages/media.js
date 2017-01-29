22a8376df29aaa687bbbfba68396260c7622c589
Thomas Preusse <tpreusse@users.noreply.github.com>
2017-01-29 21:25:34 +0100

    improve media list

--- pages/media.js
+++ pages/media.js
@@ -12,9 +12,12 @@ export default () => {
       </Head>
       <h1>Medieninformation</h1>
       
-      <p>
-        <Link href='/media/2017-01-10-hotel-rothaus'><a>Project R stellt Aufbau-Crew vor und zieht ins Hotel Rothaus</a></Link>
-      </p>
+      <ul>
+        <li>
+          Zürich, 10. Januar 2017<br />
+          <Link href='/media/2017-01-10-hotel-rothaus'><a>Project R stellt Aufbau-Crew vor und zieht ins Hotel Rothaus</a></Link>
+        </li>
+      </ul>
 
       <p>Finden Sie mehr unter: <Link href='/'><a>project-r.construction</a></Link></p>
     </Layout>
