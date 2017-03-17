126e2893d1179e3d56e0623f6a45b063d4271d7f
Clara <clara.vuillemin@posteo.de>
2017-03-17 16:56:12 +0100

    add media-links and some corrections

--- pages/jobs.js
+++ pages/jobs.js
@@ -130,11 +130,11 @@ export default ({url}) => {
         ]}
         alt='Thomas, Clara und Patrick lachen in die Kamera' />
 
-      <p>Thomas Preusse (links auf dem Bild): Frontend Engineer, ehemals Interactiv&nbsp;Things und NZZ. Begeistert von Daten-Visualisierungen und ihren journalistischen Anwendungen. Versucht das Leben selbst in einen Bubble-Chart zu verwandeln. Verwendet nie eine Technologie, die schon in Beta ist.</p>
+      <p>Thomas Preusse (links auf dem Bild): Frontend Engineer, ehemals Interactive&nbsp;Things und NZZ. Begeistert von Daten-Visualisierungen und ihren journalistischen Anwendungen. Versucht das Leben selbst in einen Bubble-Chart zu verwandeln. Verwendet nie eine Technologie, die schon in Beta ist.</p>
 
       <p>Patrick Recher (rechts): Backend-Engineer und Sysadmin, skaliert dein App von null auf zwanzig Pods in sechs Sekunden (haben wir gerade gemessen). Scheint eine Obsession für referentielle Integrität zu haben, kompensiert dies aber mit JavaScript im Backend. Immer in seinem Zimmer, trotzdem ein Nomade.</p>
 
-      <p>Clara Vuillemin: die Chefin, die coden kann. Wenn du sagst es sei unmöglich, schreibt sie übers Wochenend einen Prototyp und beweist das Gegenteil. Zu jung (oder smart), um zu wissen «wie man es richtig macht» und vertraut dem gesunden Menschenverstand. Hat tatsächlich einen Uni-Abschluss (in Elektrotechnik).</p>
+      <p>Clara Vuillemin: die Chefin, die coden kann. Wenn du sagst es sei unmöglich, schreibt sie übers Wochenend einen Prototyp und beweist das Gegenteil. Zu jung (oder smart), um zu wissen «wie man es richtig macht», vertraut dem gesunden Menschenverstand. Hat tatsächlich einen Uni-Abschluss (in Elektrotechnik).</p>
 
       <p>
         Bewerbungen bitte an: {clara.name}<br />
--- pages/media.js
+++ pages/media.js
@@ -45,9 +45,16 @@ export default ({url}) => {
       <h3>Medienartikel zu Project R</h3>
 
       <ul>
-        <li><a href='http://www.persoenlich.com/medien/wir-wollen-unser-baby-wach-intelligent-ohne-bullshit'>«Wir wollen unser Baby wach, intelligent, ohne Bullshit»</a> <em>Oktober 2016</em></li>
-        <li><a href='http://www.jungejournalisten.ch/blog/2016/10/27/und-ewig-sparen-die-verleger'>Und ewig sparen die Verleger</a> <em>Oktober 2016</em></li>
-        <li><a href='http://presseverein.ch/2016/10/09/die-rebellion-beginnt/'>Die Rebellion beginnt</a> <em>Oktober 2016</em></li>
+      	<li><a href='https://www.nzz.ch/feuilleton/medien/in-medias-ras-ein-testfall-fuer-schweizer-journalisten-ld.151509'>Die Idee von Project R liegt in der Luft</a> — NZZ, März 2017</li>
+      	<li><a href='http://www.srf.ch/sendungen/medientalk/medientalk-wie-gruende-ich-ein-medien-startup'>«Mit Fake News, mit Trump, mit dem langsamen Sterben, dem Niedergang und dem Vor-sich-her-Faulen der grossen Verlage, ist eigentlich klar, dass man etwas tun muss»</a> — Radio SRF4, Januar 2017</li>
+      	<li><a href='http://www.kontextwochenzeitung.de/medien/304/r-wie-rebellion-4158.html'>R wie Rebellion</a>  —  Kontext, Januar 2017</li>
+      	<li><a href='https://tsri.ch/zh/projectr/#.WICVuI0axL4.facebook'>«Wenn die Verleger aus der Publizistik ausgestiegen sind, ist es zu spät»</a> — tsüri.ch, Januar 2017</li>
+      	<li><a href='http://www.persoenlich.com/medien/von-den-konigen-zu-den-rebellen'>Verleger vs. Project R: Von den Königen zu den Rebellen</a> — persönlich.com, Januar 2017</li>
+      	<li><a href='https://www.nzz.ch/feuilleton/medien/journalismusprojekt-r-news-aus-dem-zuercher-rotlicht-viertel-ld.139282'>News aus dem Zürcher Rotlichtviertel</a> — NZZ, Januar 2017</li>
+      	<li><a href='http://www.deutschlandradiokultur.de/schweizer-online-magazin-project-r-die-verlage-verwandeln.1013.de.html?dram:article_id=376069'>«Die Verlage verwandeln sich in Internet-Handelshäuser»</a> — Deutschlandradio Kultur, Januar 2017</li>
+        <li><a href='http://www.persoenlich.com/medien/wir-wollen-unser-baby-wach-intelligent-ohne-bullshit'>«Wir wollen unser Baby wach, intelligent, ohne Bullshit»</a> — persönlich.com, Oktober 2016</li>
+        <li><a href='http://www.jungejournalisten.ch/blog/2016/10/27/und-ewig-sparen-die-verleger'>Und ewig sparen die Verleger</a> — jungejournalisten.ch, Oktober 2016</li>
+        <li><a href='http://presseverein.ch/2016/10/09/die-rebellion-beginnt/'>Die Rebellion beginnt</a> — presseverein.ch, Oktober 2016</li>
       </ul>
     </Layout>
   )
