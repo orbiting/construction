c6499c08ed3f8ba6a7d76071b79f51e8b1b727fc
Thomas Preusse <tpreusse@users.noreply.github.com>
2017-05-31 19:18:15 +0200

    code clean

--- pages/newsletter/2017-05-31-signal-journalismus.js
+++ pages/newsletter/2017-05-31-signal-journalismus.js
@@ -1,23 +1,7 @@
 import React from 'react'
-import {css} from 'glamor'
 
 import Layout from '../../src/Layout'
 import Cover from '../../src/Cover'
-import Newsletter from '../../src/Newsletter'
-
-const topBreak = 700
-const topPromoStyle = css({
-  padding: '0 0 20px 20px',
-  maxWidth: 280,
-  fontSize: 14,
-  float: 'right',
-  [`@media (max-width: ${topBreak}px)`]: {
-    display: 'none'
-  },
-  '@media (min-width: 800px)': {
-    marginRight: -80
-  }
-})
 
 export default ({url}) => {
   const meta = {
