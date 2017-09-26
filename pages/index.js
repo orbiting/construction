fe22e8db066603adf7476cf39edcf4f922afe7b5
Daniel Pfänder <daniel.pfaender@project-r.construction>
2017-09-26 17:48:52 +0200

    Load published (newsletter) documents on construction homepage from CMS API

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,8 @@
-import React from 'react'
+import React, { Component } from 'react'
+
+import { gql, graphql } from 'react-apollo'
+import { compose } from 'redux'
+import withData from '../lib/apollo/withData'
 
 import Layout from '../src/Layout'
 import Newsletter from '../src/Newsletter'
@@ -6,7 +10,20 @@ import Cover from '../src/Cover'
 import Card from '../src/Card'
 import Grid, {GridItem} from '../src/Grid'
 
-const newsletters = [
+const getDocuments = gql`
+  query getDocuments {
+    documents {
+      content
+      meta {
+        title
+        description
+        image
+      }
+    }
+  }
+`
+
+const staticNewsletters = [
   {
     href: '/newsletter/2017-09-13-open-source',
     title: 'Der Crowdfunding-Code gegen die Frankenstein-Monster-Strategie',
@@ -72,51 +89,77 @@ const newsletters = [
   }
 ]
 
-export default ({url}) => {
-  const meta = {
-    title: 'Project R geht an den Start',
-    description: '«Es ist Zeit, dass sich die Journalisten unabhängig machen und der Journalismus unabhängig von den Grossverlagen existieren kann. Und ein Modell dafür schafft man nur gemeinsam – oder gar nicht.»',
-    image: 'https://assets.project-r.construction/images/balkon.jpg',
-    url: `https://project-r.construction${url.pathname}`
+class Index extends Component {
+  render () {
+    const { data: {loading, error, documents}, url } = this.props
+
+    const meta = {
+      title: 'Project R geht an den Start',
+      description: '«Es ist Zeit, dass sich die Journalisten unabhängig machen und der Journalismus unabhängig von den Grossverlagen existieren kann. Und ein Modell dafür schafft man nur gemeinsam – oder gar nicht.»',
+      image: 'https://assets.project-r.construction/images/balkon.jpg',
+      url: `https://project-r.construction${url.pathname}`
+    }
+
+    let newsletters = [...staticNewsletters]
+
+    // TODO: Copy over and use Loader here.
+    if (!loading && !error) {
+      documents.forEach(document => {
+        newsletters.unshift({
+          href: '/',  // TODO: Get via API.
+          title: document.meta.title,
+          date: '31. Dezember 2017',  // TODO: Get via API.
+          image: {
+            src: document.meta.image,
+            alt: ''  // TODO: Get via API.
+          }
+        })
+      })
+    }
+
+    return (
+      <Layout meta={meta} url={url} cover={(
+        <Cover image={{
+          src: 'https://assets.project-r.construction/images/rothaus_r.jpg',
+          alt: 'Balkon vom Hotel Rothaus mit gehisstem Project R Logo'}}
+        >
+          <h1>Project R</h1>
+          <p>{meta.description}</p>
+        </Cover>
+      )}>
+
+        <p>Ladies and Gentlemen,</p>
+
+        <p>Thomas Jefferson, der Autor der amerikanischen Unabhängigkeits­erklärung, schrieb einmal:</p>
+
+        <blockquote>«Ich bin sicher, eine kleine Rebellion hie und da ist eine gute Sache; sie ist in der Politik so notwendig, um die Dinge zu klären, wie ein Sturm für das Wetter.»</blockquote>
+
+        <p>Wir glauben, dass auch im Journalismus die Zeit für eine kleine Rebellion gekommen ist. Und arbeiten daran, sie zu machen.</p>
+
+        <p>Vom 26. April bis 31. Mai lief die Crowdfunding-Kampagne für das digitale Magazin «Republik» von Project R. Ein überwältigender Erfolg: Über 13'000 Unterstützerinnen und Unterstützer wollen gemeinsam mit der Aufbau-Crew von Project R im Mediensystem einen entscheidenden Unterschied schaffen. Die «Republik» geht 2018 an den Start – kompromisslos in der Qualität, leserfinanziert, ohne Werbung und ohne Bullshit.</p>
+
+        <p>Hier finden Sie die «Republik» <a href='https://www.republik.ch'>www.republik.ch</a>. Die Webseite des erfolgreichen Crowdfundings finden Sie <a href='https://www.republik.ch/crowdfunding'>hier</a>.</p>
+
+        <p>Wenn Sie informiert bleiben wollen, bitte hier für den Newsletter eintragen:</p>
+        <Newsletter />
+
+        <p>Danke!</p>
+
+        <h3>Aktuelles von Project R</h3>
+
+        <Grid>
+          {newsletters.map((newsletter, i) => (
+            <GridItem key={i}>
+              <Card {...newsletter} />
+            </GridItem>
+          ))}
+        </Grid>
+      </Layout>
+    )
   }
-
-  return (
-    <Layout meta={meta} url={url} cover={(
-      <Cover image={{
-        src: 'https://assets.project-r.construction/images/rothaus_r.jpg',
-        alt: 'Balkon vom Hotel Rothaus mit gehisstem Project R Logo'}}
-      >
-        <h1>Project R</h1>
-        <p>{meta.description}</p>
-      </Cover>
-    )}>
-
-      <p>Ladies and Gentlemen,</p>
-
-      <p>Thomas Jefferson, der Autor der amerikanischen Unabhängigkeits­erklärung, schrieb einmal:</p>
-
-      <blockquote>«Ich bin sicher, eine kleine Rebellion hie und da ist eine gute Sache; sie ist in der Politik so notwendig, um die Dinge zu klären, wie ein Sturm für das Wetter.»</blockquote>
-
-      <p>Wir glauben, dass auch im Journalismus die Zeit für eine kleine Rebellion gekommen ist. Und arbeiten daran, sie zu machen.</p>
-
-      <p>Vom 26. April bis 31. Mai lief die Crowdfunding-Kampagne für das digitale Magazin «Republik» von Project R. Ein überwältigender Erfolg: Über 13'000 Unterstützerinnen und Unterstützer wollen gemeinsam mit der Aufbau-Crew von Project R im Mediensystem einen entscheidenden Unterschied schaffen. Die «Republik» geht 2018 an den Start – kompromisslos in der Qualität, leserfinanziert, ohne Werbung und ohne Bullshit.</p>
-
-      <p>Hier finden Sie die «Republik» <a href='https://www.republik.ch'>www.republik.ch</a>. Die Webseite des erfolgreichen Crowdfundings finden Sie <a href='https://www.republik.ch/crowdfunding'>hier</a>.</p>
-
-      <p>Wenn Sie informiert bleiben wollen, bitte hier für den Newsletter eintragen:</p>
-      <Newsletter />
-
-      <p>Danke!</p>
-
-      <h3>Aktuelles von Project R</h3>
-
-      <Grid>
-        {newsletters.map((newsletter, i) => (
-          <GridItem key={i}>
-            <Card {...newsletter} />
-          </GridItem>
-        ))}
-      </Grid>
-    </Layout>
-  )
 }
+
+export default compose(
+  withData,
+  graphql(getDocuments)
+)(Index)
