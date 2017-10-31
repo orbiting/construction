6f65447703434c7788adae9f1de0cfc5b3175968
Thomas Preusse <tpreusse@users.noreply.github.com>
2017-10-31 17:29:53 +0100

    use prop types package

--- src/Layout.js
+++ src/Layout.js
@@ -1,4 +1,5 @@
-import React, {PropTypes} from 'react'
+import React from 'react'
+import PropTypes from 'prop-types'
 import Head from 'next/head'
 import {css} from 'glamor'
 import Footer from './Footer'
