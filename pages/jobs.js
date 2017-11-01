f24ad9e49f86eabed0f432befcd3a41483f6e2dc
patte <p@tte.io>
2017-11-01 23:00:45 +0100

    fix indents

--- pages/jobs.js
+++ pages/jobs.js
@@ -17,8 +17,8 @@ export default ({url}) => {
         src: meta.image,
         alt: 'Fünf Mitarbeiter der «Republik» im Büro'}}
       >
-        <h1>Wir bilden aus!</h1>
-        <p>Löse mit einem motivierten, multidisziplinären und unorthodoxen Team die interessantesten Probleme der Branche!</p>
+      <h1>Wir bilden aus!</h1>
+      <p>Löse mit einem motivierten, multidisziplinären und unorthodoxen Team die interessantesten Probleme der Branche!</p>
       </Cover>
     )}>
 
@@ -27,7 +27,7 @@ export default ({url}) => {
     <p><strong>Wir suchen:</strong> Leute, die anders sind. Die viel können und noch viel mehr vorhaben. Die laut sind und nicht einverstanden sind, die analytisch denken können und ihre Meinung eloquent artikulieren. Wir wollen eine diverse Republik. Mit verschiedenen Herkünften und Erfahrungswelten, mit eigenwilligen Ausdrucksformen, mit Stimmen, die sich unterscheiden. Wir suchen ehrgeizige Schreiber, die unternehmerisch denken. Wir suchen angehende Unternehmerinnen, die für guten Journalismus brennen. </p>
 
     <p>Sichere Medienjobs werden bald so selten sein wie optimistische Verlagsmanager. Das ist die schlechte Nachricht. Die gute: Die Investitionen dafür, ein Medienunternehmen zu gründen, sind heute so gering wie niemals zuvor, seit Reinhard Lutz 1571 seine «Warhafftige Zeitung von den Gottlosen Hexen» herausgab, eines der ersten Presseorgane der Neuzeit. Alles, was du heute brauchst, sind eine bestechende Idee, ehrgeizige Mitstreiter, Know-how und ein mutiges Herz. Und los. Es wird Zeit, dass wir unsere eigenen Medien gründen. Die Zeiten für Verlegerinnen und Verleger könnten nicht besser sein.</p>
-    
+
     <p>Darum gibt es diese Ausbildung. Sie beginnt am Montag, 7. Mai 2018, dauert zwölf Monate und macht dich fit für die digitale Medienzukunft. Sie ist flexibel und auf deine Bedürfnisse zugeschnitten. Mit uns gemeinsam stellst du am Anfang aus einem Baukasten von Modulen deinen eigenen Ausbildungsplan zusammen.</p>
 
     <p>Auf der unternehmerischen Seite kannst du die Abteilungen Geschäftsführung, Community, Events, Marketing, IT-Entwicklung und Project R Lab durchlaufen – und wirst viel lernen über Projektmanagement, Mitarbeiterführung, Buchführung, Management etc. Du blickst ins Räderwerk eines leserzentrierten, mitgliederbasierten Webmagazins. Lernst, wie man in einen Markt hineinhorcht und einen marktreifen Prototyp entwickelt.</p>
@@ -49,17 +49,17 @@ export default ({url}) => {
     <p>Diese Bewerbung enthält:</p>
     <ul>
 
-      <li>ein «Motivationsschreiben» in Form eines einminütigen Videos. Zeig uns, wer du bist und warum du zu uns passt. Was dir auf den Nägeln brennt. Lass dir etwas einfallen. Überrasch uns!</li>
-      <li>einen tabellarischen Lebenslauf.</li>
-      <li>ein bis drei Arbeitsproben. Von Text bis Multimedia bis Projekt. Einen Verweis auf die ein bis drei Dinge, die dir am besten gelungen sind.</li>
-      <li>Bitte schreib einen Essay/Kommentar zu einer politischen Frage, die dich umtreibt. Er endet mit der Versicherung: «Ich habe diesen Text allein und ohne Hilfe von Dritten geschrieben.» Oder: Skizziere eine Idee für ein journalistisches Start-up, inklusive eines rudimentären Businessplans. (Nur eines der beiden. Entweder – oder. Bitte höchstens 5000 Zeichen).</li>
-  </ul>
+    <li>ein «Motivationsschreiben» in Form eines einminütigen Videos. Zeig uns, wer du bist und warum du zu uns passt. Was dir auf den Nägeln brennt. Lass dir etwas einfallen. Überrasch uns!</li>
+    <li>einen tabellarischen Lebenslauf.</li>
+    <li>ein bis drei Arbeitsproben. Von Text bis Multimedia bis Projekt. Einen Verweis auf die ein bis drei Dinge, die dir am besten gelungen sind.</li>
+    <li>Bitte schreib einen Essay/Kommentar zu einer politischen Frage, die dich umtreibt. Er endet mit der Versicherung: «Ich habe diesen Text allein und ohne Hilfe von Dritten geschrieben.» Oder: Skizziere eine Idee für ein journalistisches Start-up, inklusive eines rudimentären Businessplans. (Nur eines der beiden. Entweder – oder. Bitte höchstens 5000 Zeichen).</li>
+    </ul>
 
-  <p>Wir melden uns Anfang Dezember bei dir. Hoffentlich mit einer Einladung zum Auswahltag. Er ist am Samstag, 16. Dezember 2017, von 8 bis 20 Uhr in Zürich.</p>
+    <p>Wir melden uns Anfang Dezember bei dir. Hoffentlich mit einer Einladung zum Auswahltag. Er ist am Samstag, 16. Dezember 2017, von 8 bis 20 Uhr in Zürich.</p>
 
-  <p>Hast du Fragen? Dann schreib an ariel.hauptmeier@republik.ch</p>
+    <p>Hast du Fragen? Dann schreib an ariel.hauptmeier@republik.ch</p>
 
-  <p>Viel Glück!</p>
+    <p>Viel Glück!</p>
 
     </Layout>
   )
