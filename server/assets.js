4e8f255685ea46c67c35707fdf15f2982c7f4b65
Patrick Venetz <venetz@gmail.com>
2022-03-10 15:36:37 +0100

    feat: pipe upstream when fetching /assets/org/…

--- server/assets.js
+++ server/assets.js
@@ -1,4 +1,5 @@
 const server = require('express').Router()
+const fetch = require('isomorphic-unfetch')
 
 const { ASSETS_REPOS_REDIRECT_TARGET } = process.env
 
@@ -11,7 +12,23 @@ server.get('/assets/*', async (req, res, next) => {
   const targetPath = originalUrl.replace(/^\/assets\/(.+?)\//, '')
   const targetUrl = new URL(targetPath, ASSETS_REPOS_REDIRECT_TARGET)
 
-  res.redirect(targetUrl)
+  await fetch(targetUrl.toString())
+    .then(targetRes => {
+      const { ok, status, statusText, headers } = targetRes
+      if (!ok) {
+        return res.status(status).end(statusText)
+      }
+
+      Array
+        .from(['Content-Type', 'Last-Modified', 'Content-Length'])
+        .forEach(key => { res.set(key, headers.get(key)) })
+
+      targetRes.body.pipe(res)
+    })
+    .catch((e) => {
+      console.error({ e, originalUrl, targetPath, ASSETS_REPOS_REDIRECT_TARGET, targetUrl })
+      return res.status(500).end('Internal Server Error')
+    })
 })
 
 module.exports = server
